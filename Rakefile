c65547a02059485b1f92e17feba1c20129ee59b9
Jamal El Milahi <jamal@elmilahi.com>
2012-10-03 08:59:38 -0700

    Set up testing environment

--- Rakefile
+++ Rakefile
@@ -1,1 +1,10 @@
 require 'bundler/gem_tasks'
+require 'rspec/core/rake_task'
+
+desc 'Default: run specs.'
+task :default => :rspec
+
+RSpec::Core::RakeTask.new(:rspec) do |test|
+  test.rspec_opts = ['--color']
+  test.pattern = './spec/**/*_spec.rb'
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,50 @@
+require 'active_record'
+require 'database_cleaner'
+require 'sqlite3'
+require 'ibham'
+
+db_config = {
+  :adapter => 'sqlite3',
+  :database => ':memory:'
+}
+ActiveRecord::Base.establish_connection(db_config)
+
+RSpec.configure do |config|
+  config.before(:each) { DatabaseCleaner.start }
+  config.after(:each) { DatabaseCleaner.clean }
+end
+
+ActiveRecord::Migration.verbose = false
+
+ActiveRecord::Schema.define do
+  create_table :votes do |t|
+    t.references :voter, polymorphic: true
+    t.references :voteable, polymorphic: true
+    t.integer :value
+
+    t.timestamps
+  end
+
+  add_index :votes, [:voter_id, :voter_type]
+  add_index :votes, [:voteable_id, :voteable_type]
+
+  create_table :users do |t|
+    t.string :email
+    t.timestamps
+  end
+
+  create_table :items do |t|
+    t.string :name
+    t.timestamps
+  end
+end
+
+class User < ActiveRecord::Base
+  has_many :items
+  acts_as_voter
+end
+
+class Item < ActiveRecord::Base
+  belongs_to :user
+  acts_as_voteable
+end
